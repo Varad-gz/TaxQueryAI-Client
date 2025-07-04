0edf6e7a136bd54df167615c1c922ebe654ba1cf
Pratyush Majumdar <pratyushmajumdar27@gmail.com>
2025-07-04 17:44:31 +0530

    changes done

--- src/app/(prelogin)/page.js
+++ src/app/(prelogin)/page.js
@@ -1,7 +1,5 @@
 'use client'
 import React from 'react'
-
-import DeveloperCard from '@/components/DeveloperCard';
 import PreLoginNavbar from '@/components/PreLoginNavbar';
 import { motion } from 'framer-motion';
 
@@ -25,22 +23,6 @@ const Home = () => {
                 >
                     We simplify indian property tax queries with our advanced querying tool. Get instant answers, detailed analytics, and 24/7 support through our integrated chatbot. Let us handle the complexity, so you don’t have to.
                 </motion.p>
-            </div>
-            <div className='p-[40px] text-center'>
-                <h3 className='text-4xl font-bold mb-[30px]'>Meet the Developers</h3>
-                <motion.div
-                    className='flex flex-col md:flex-row justify-center space-y-10 md:space-x-10'
-                    initial={{ opacity: 0, y: 50 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 1 }}
-                    viewport={{ once: true }}
-                >
-                    <DeveloperCard name="Varad Pandit" githubLink='https://github.com/Varad-gz' linkedinLink='https://www.linkedin.com/in/pandit-varad' />
-                    <DeveloperCard name="Pratyush Majumdar" githubLink='https://github.com/pratyush770' linkedinLink='https://www.linkedin.com/in/pratyush-majumdar' />
-                </motion.div>
-            </div>
-            <div className='bg-zinc-900 h-[100px] text-white flex items-center justify-center'>
-                <span>TaxQueryAI 2025</span>
             </div>
         </div>
     )
